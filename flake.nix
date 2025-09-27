4c3b2dc0202fbb481c917da1a6efb103be889a3a
Kirk Matt <kirkmatt@proton.me>
2025-09-27 19:21:56 +0100

    added flake and changed fontend to tkinter

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+    description = "Flake for Uom Canvas Scraper";
+
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    };
+
+    outputs = { self, nixpkgs }:
+    let
+        pkgs = nixpkgs.legacyPackages."x86_64-linux";
+    in
+    {
+        devShells."x86_64-linux".default = pkgs.mkShell {
+            packages = with pkgs; [
+                    python313Packages.tkinter
+                ];
+        };
+    };
+}
